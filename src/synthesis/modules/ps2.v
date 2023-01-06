2e35d51b60671e7272723edb89f27979d3061037
Andrija Stojanovic <stojanovicandrija123@gmail.com>
2023-01-06 20:59:00 +0100

    Koncept 2

--- src/synthesis/modules/ps2.v
+++ src/synthesis/modules/ps2.v
@@ -3,32 +3,34 @@ module ps2(
     input kbclk,
     input rst_n,
     input in,
-    output [6:0] out
+    output [6:0] out0,
+    output [6:0] out1,
 );
 
     wire deb_kbclk;
     deb deb_inst(.clk(clk), .rst_n(rst_n), .in(kbclk), .out(deb_kbclk));
 
     reg [7:0] data_reg, data_next;
-    reg [3:0] display_reg, display_next;
+    reg [3:0] display_reg0;
+    reg [3:0] display_reg1;
 
-    hex hex_inst(.in(display_reg), .out(out))
+    assign display_reg0 = data_reg[3:0];
+    assign display_reg1 = data_reg[7:4];
+
+    hex hex_inst0(.in(display_reg0), .out(out0));
+    hex hex_inst1(.in(display_reg1), .out(out1));
 
     always @(posedge clk, negedge rst_n) begin
         if(!rst_n) begin
-            display_reg <= 4'h0;
             data_reg <= 8'h00;
         end
         else begin
-            display_reg <= display_next;
             data_reg <= data_next;
         end
     end
 
     always(*) begin
-
-        display_next = display_reg;
-        data_next = data_reg
+        data_next = data_reg;
 
         
     end
