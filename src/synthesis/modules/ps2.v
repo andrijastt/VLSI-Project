9cce0d3fd45a3535a37e5144e3d725d7fa9e367e
Andrija Stojanovic <stojanovicandrija123@gmail.com>
2023-01-06 20:32:56 +0100

    Koncept za ps2

--- src/synthesis/modules/ps2.v
+++ src/synthesis/modules/ps2.v
@@ -1,3 +1,37 @@
-module ps2;
+module ps2(
+    input clk,
+    input kbclk,
+    input rst_n,
+    input in,
+    output [6:0] out
+);
+
+    wire deb_kbclk;
+    deb deb_inst(.clk(clk), .rst_n(rst_n), .in(kbclk), .out(deb_kbclk));
+
+    reg [7:0] data_reg, data_next;
+    reg [3:0] display_reg, display_next;
+
+    hex hex_inst(.in(display_reg), .out(out))
+
+    always @(posedge clk, negedge rst_n) begin
+        if(!rst_n) begin
+            display_reg <= 4'h0;
+            data_reg <= 8'h00;
+        end
+        else begin
+            display_reg <= display_next;
+            data_reg <= data_next;
+        end
+    end
+
+    always(*) begin
+
+        display_next = display_reg;
+        data_next = data_reg
+
+        
+    end
+
 
 endmodule
